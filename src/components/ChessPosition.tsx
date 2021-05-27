7974309640bdc32755246ec2d1e3c648ab638737
p-barski <patryk.kubarski@protonmail.com>
2021-05-27 09:08:06 +0200

    Changed colors in ChessPosition

--- src/components/ChessPosition.tsx
+++ src/components/ChessPosition.tsx
@@ -8,22 +8,6 @@ import { ChessColor } from "../Chess/ChessEnums/ChessColor";
 
 const ChessLookup = (piece: ChessPieceWrapper | undefined): string => {
 	const type = piece?.ChessPiece.PieceType;
-	if (piece?.ChessPiece.Color === ChessColor.White) {
-		switch (type) {
-			case ChessPieceType.King:
-				return "♔";
-			case ChessPieceType.Queen:
-				return "♕";
-			case ChessPieceType.Rook:
-				return "♖";
-			case ChessPieceType.Bishop:
-				return "♗";
-			case ChessPieceType.Knight:
-				return "♘";
-			case ChessPieceType.Pawn:
-				return "♙";
-		}
-	}
 	switch (type) {
 		case ChessPieceType.King:
 			return "♚";
@@ -50,11 +34,14 @@ interface Props {
 
 const ChessPosition = (props: Props) => {
 	const colorStyle = props.isWhite ? styles.white : styles.black;
+	const textColor = props.piece?.ChessPiece.Color == ChessColor.White ? styles.textWhite : styles.textBlack;
 	const style = [styles.default, colorStyle];
+	const textStyle = [styles.text, textColor];
 	const selectedPiece = useStoreState(state => state.selectedPiece);
 	const isDestinationPosition = useStoreState(state => state.possibleMoves)
 		.filter(m => arePositionsTheSame(m.ChessMove.FinishedPosition, props.position))
 		.length !== 0;
+
 	if (selectedPiece !== undefined && selectedPiece === props.piece) {
 		style[1] = styles.selected;
 	}
@@ -72,12 +59,18 @@ const ChessPosition = (props: Props) => {
 	const str = ChessLookup(props.piece);
 	return (
 		<View onStartShouldSetResponder={onPress} style={style}>
-			<Text style={styles.text}>{str}</Text>
+			<Text style={textStyle}>{str}</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
+	textWhite: {
+		color: "white",
+	},
+	textBlack: {
+		color: "black",
+	},
 	text: {
 		textAlign: "center",
 		textAlignVertical: "center",
@@ -95,10 +88,10 @@ const styles = StyleSheet.create({
 		backgroundColor: "#2e2e2e",
 	},
 	selected: {
-		backgroundColor: "magenta"
+		backgroundColor: "#6e6e9e"
 	},
 	destination: {
-		backgroundColor: "aqua"
+		backgroundColor: "#9e6e6e"
 	}
 });
 export default ChessPosition;
